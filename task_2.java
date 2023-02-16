316f3bb6fa5adfc255e92f847579483687614d33
MariannaSarkisian <spn.mjj@gmail.com>
2023-02-17 00:07:58 +0300

    2z

--- task_2.java
+++ task_2.java
@@ -1,4 +1,16 @@
 //Вывести все простые числа от 1 до 1000
 public class task_2 {
-    
-}
+     public static void main(String[] args) {
+        int n = 1000;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 2; j <= Math.sqrt(i); j++) { //квадратный корень индекса
+                if (i % j == 0) {
+                    break;
+                }
+            }
+                System.out.println(i);
+            }
+        }
+    }
+
+
